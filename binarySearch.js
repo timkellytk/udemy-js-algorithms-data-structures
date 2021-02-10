1e1c35fdc82e3d15d24b648d4b130854c3df8254
Tim Kelly <tim.kelly@sca.com.au>
2021-02-10 13:49:33 +1100

    Completed binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -1,5 +1,31 @@
 function binarySearch(arr, expected) {
-    return true;
+    let bottom = 0;
+    let top = arr.length - 1;
+
+    while (true) {
+        let middle = Math.floor(bottom + (top - bottom) / 2);
+        let value = arr[middle]
+
+        // If value is found
+        if (value === expected) {
+            return middle;
+        }
+        // If value is not found and no remaining options
+        if (top === bottom) {
+            break;
+        }
+
+        // If value > expected set top to middle - 1
+        if (value > expected) {
+            top = middle - 1;
+        }
+        
+        // If value < expected set bottom to middle + 1
+        if (value < expected) {
+            bottom = middle + 1;
+        }
+    }
+    return null;
 }
 
 module.exports = binarySearch;
--- binarySearch.test.js
+++ binarySearch.test.js
@@ -1,5 +1,19 @@
 const binarySearch = require("./binarySearch");
 
-test("example", () => {
-  expect(binarySearch()).toEqual(true);
+describe('binarySearch - return the correct index of value', () => { 
+  test("binarySearch([1,2,3,4,5],2) should be 1.", () => {
+    expect(binarySearch([1,2,3,4,5],2)).toEqual(1);
+  });
+  test('binarySearch([1,2,3,4,5],3) should be 2.', () => {
+    expect(binarySearch([1, 2, 3, 4, 5], 3)).toEqual(2);
+  });
+  test('binarySearch([1,2,3,4,5],5) should be 4.', () => {
+    expect(binarySearch([1,2,3,4,5],5)).toEqual(4);
+  });
+  test('binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10) should be 2.', () => {
+    expect(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10)).toEqual(2);
+  });
+  test('binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95) should be 16.', () => {
+    expect(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95)).toEqual(16);
+  });
 });
